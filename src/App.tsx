5a2c9809c8784780b1f44027beb330bdeca86ddc
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-06-21 18:28:32 +0900

    Bootstrap導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { UserDetail } from "./UserDetail";
+import { Breadcrumb } from "./components/Breadcrumb";
 
 const App = () => {
   return (
     <div>
+      <Breadcrumb />
       <UserDetail />
     </div>
-  )
+  );
 };
 
 export default App;
