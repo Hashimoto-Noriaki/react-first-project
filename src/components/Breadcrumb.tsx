77ddd49027850cd44371941e4807be035240ac59
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-06-21 18:34:01 +0900

    Fragment

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -1,18 +1,22 @@
 export const Breadcrumb = () => {
     return (
-        <nav aria-label="breadcrumb">
-        <ol className="breadcrumb">
-            <li className="breadcrumb-item">
-            <a href="#">Home</a>
-            </li>
-            <li className="breadcrumb-item">
-            <a href="#">Library</a>
-            </li>
-            <li className="breadcrumb-item">
-            <a href="#">Date</a>
-            </li>
-        </ol>
-        </nav>
+        <>
+            <p>パンくずリスト</p>
+            <nav aria-label="breadcrumb">
+            <ol className="breadcrumb">
+                <li className="breadcrumb-item">
+                <a href="#">Home</a>
+                </li>
+                
+                <li className="breadcrumb-item">
+                <a href="#">Library</a>
+                </li>
+                <li className="breadcrumb-item">
+                <a href="#">Date</a>
+                </li>
+            </ol>
+            </nav>
+            </>
     );
-    };
+};
 
