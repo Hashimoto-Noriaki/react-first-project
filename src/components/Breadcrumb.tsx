9b6bdc3e050a47ed3e4a169069c9c4796c390d9c
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-06-21 19:03:09 +0900

    onClick

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -1,23 +1,31 @@
-export const Breadcrumb = () => {
-    const items = ["Home","Library","Date"]
-    return (
-        <>
-            <p>パンくずリスト</p>
-            {items.length === 0 ?(
-                <p>itemsがありません</p>
-            ): (
-                <p>itemが{items.length}あります</p>
-            )}
-            <nav aria-label="breadcrumb">
-                <ol className="breadcrumb">
-                    {items.map((item) =>(
-                        <li key={item} className="breadcrumb-items">
-                            <a href = "#">{item}</a>
-                        </li>
-                    ))}
-                </ol>
-            </nav>
-        </>
-    );
-};
+import { MouseEvent } from "react";
 
+export const Breadcrumb = () => {
+  const items = ["Home", "Library", "Date"];
+
+  const handleClick = (event: MouseEvent<HTMLAnchorElement>) => {
+      console.log(event);
+  }
+
+  return (
+    <>
+      <p>パンくずリスト</p>
+      {items.length === 0 ? (
+        <p>itemがありません。</p>
+      ) : (
+        <p>itemが{items.length}あります</p>
+      )}
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          {items.map((item) => (
+            <li key={item} className="breadcrumb-item">
+              <a href="#" onClick={handleClick}>
+                {item}
+              </a>
+            </li>
+          ))}
+        </ol>
+      </nav>
+    </>
+  );
+};
